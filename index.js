43f7d5fe6dd39a6e09b4d17dd1d8e79ae784325e
OM Miles <olivia.m.miles@gmail.com>
2021-04-07 13:05:37 -0700

    Completed lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    title = title.split(` `).map(word => (word.charAt(0).toUpperCase() + word.slice(1)));
+    return title.join(` `);
+  })
 }
+
+console.log(titleCased(tutorials))
